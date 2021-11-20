8ec55b0c04bed0ef0ed3ca453d5d0e2c1340eb02
Duncan McClean <duncan@mcclean.co.uk>
2021-11-20 17:23:10 +0000

    wip

--- dist/index.js
+++ dist/index.js
@@ -8289,13 +8289,17 @@ exports.main = async function main() {
         // const issueReferenceExpression = /(?:(?<![/\w-.])\w[\w-.]+?\/\w[\w-.]+?|\B)#[1-9]\d*?\b/g // This one supports things like: doublethreedigital/runway#641
         const issueReferenceExpression = /(?<![a-zA-Z])#[1-9]\d*?\b/g // Whereas, this just supports #641
 
-        changelogBody.match(issueReferenceExpression).forEach((reference) => {
-            octokit.rest.issues.createComment({
-                ...github.context.repo,
-                issue_number: reference.replace('#', ''),
-                body: `Released as part of [${version}](https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/releases/tag/${version}).`,
-            })
-        })
+        core.info(changelogBody)
+        core.info(issueReferenceExpression)
+        core.info(changelogBody.match(issueReferenceExpression))
+
+        // changelogBody.match(issueReferenceExpression).forEach((reference) => {
+        //     octokit.rest.issues.createComment({
+        //         ...github.context.repo,
+        //         issue_number: reference.replace('#', ''),
+        //         body: `Released as part of [${version}](https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/releases/tag/${version}).`,
+        //     })
+        // })
 
         core.info("All Done!")
     } catch (error) {
