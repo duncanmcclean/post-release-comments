9b6d479b1ff1b291dbf218a51603ffffba6d8488
Duncan McClean <duncan@mcclean.co.uk>
2021-11-20 13:21:28 +0000

    Use context for the issue creating if we ever get this thing working

--- src/main.js
+++ src/main.js
@@ -11,20 +11,15 @@ exports.main = async function main() {
         // const issueReferenceExpression = /(?:(?<![/\w-.])\w[\w-.]+?\/\w[\w-.]+?|\B)#[1-9]\d*?\b/g // This one supports things like: doublethreedigital/runway#641
         const issueReferenceExpression = /(?<![a-zA-Z])#[1-9]\d*?\b/g // Whereas, this just supports #641
 
-        let references = changelogBody.match(issueReferenceExpression)
-
-        // const context = github.context;
-
-        references.forEach((reference) => {
+        changelogBody.match(issueReferenceExpression).forEach((reference) => {
             octokit.rest.issues.createComment({
-                owner: 'doublethreedigital',
-                repo: 'gh-actions-release-workflow-test',
+                ...github.context.repo,
                 issue_number: reference.replace('#', ''),
-                body: `Released as part of ${version}. Hope you have a great rest of your day!`,
+                body: `Released as part of [${version}](https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/releases/tag/${version}).`,
             })
         })
 
-        core.info("Done all the things by this point.")
+        core.info("All Done!")
     } catch (error) {
         core.setFailed(error.message)
     }
